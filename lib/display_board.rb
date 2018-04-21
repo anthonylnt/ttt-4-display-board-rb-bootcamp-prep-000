9f53827eba64659bb7ef9a86cde9af81914b2dab
Anthony Elijah <lionnet.ant@gmail.com>
2018-04-21 21:36:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,3 @@ def display_board(rows)
   puts " #{rows[0]} | #{rows[1]} | #{rows[2]} \n-----------\n #{rows[3]} | #{rows[4]} | #{rows[5]} \n-----------\n #{rows[6]} | #{rows[7]} | #{rows[8]} "
 end
 
-board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
-display_board(board)
